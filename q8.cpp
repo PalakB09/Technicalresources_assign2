1fdbd2e226f1d4a9862c05dd8e0e15157fd2011e
PalakB09 <117338984+PalakB09@users.noreply.github.com>
2023-01-22 19:19:54 +0530

    Create q8.cpp

---
+++ q8.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"number of rows you want to print";
+    cin>>num;
+    for(int i=1;i<=num;i++){
+        for(int j=1;j<num;j++){
+            if(j+i==6){
+                cout<<"*";
+            }
+            cout<<" ";
+
+        }
+  
+        cout<<endl;
+        
+        
+    }
+    return 0;
+}   
